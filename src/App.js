3540fb373cdaeafac5ab0d3b60c48cb1d4cac8a6
sudais <sudais.shafiq22@gmail.com>
2023-02-28 19:32:26 +0500

    project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './list';
+import { useState } from 'react';
 
 function App() {
+  const [itemsArray, setItemsArray] = useState(data);
+  const [newItem, setNewItem] = useState('');
+
+  const handleClick = e =>{
+    e.preventDefault();
+    setItemsArray((prevArray)=>{
+      return [...prevArray , newItem ]
+    });
+    setNewItem('');
+  }
+
+  const handleChange = e =>{
+    setNewItem(e.target.value);
+  }
+
+  const handleDelete = (e) =>{
+     setItemsArray( itemsArray.filter(item=> item !== e.target.innerText))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+          <h1>Simple Todo</h1>
+          <p className='list-items-count'>{itemsArray.length} items in the list</p>
+          <ul>
+          {itemsArray.map(list=>{
+            return <li onClick={handleDelete}>{list}</li>
+          })}
+          </ul>
+          <form onSubmit={handleClick}>
+            <div className='d-flex'>
+                  <input onChange={handleChange} value={newItem} type="text" name="new-item" id="newItem" required />
+                  <button className='btn-add' type='submit'>Add</button>
+            </div>
+          </form>
+      </div>
     </div>
   );
 }
---
+++ src/list.js
@@ -0,0 +1,8 @@
+const data  = [
+    "Clean my house",
+    "Get Medicine",
+    "Check Emails",
+    "Send Proposals",
+]
+
+export default data;
